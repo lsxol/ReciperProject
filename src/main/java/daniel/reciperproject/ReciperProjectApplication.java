1353c57dfb2bf7170bd3bff5f0eec5d46f7b3a6d
lsxol <d.kazmierczak96@gmail.com>
2022-08-10 10:40:52 +0200

    Init

---
+++ src/main/java/daniel/reciperproject/ReciperProjectApplication.java
@@ -0,0 +1,13 @@
+package daniel.reciperproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciperProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReciperProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/daniel/reciperproject/ReciperProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package daniel.reciperproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciperProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
