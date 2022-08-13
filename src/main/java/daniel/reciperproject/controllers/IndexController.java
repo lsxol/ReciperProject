be55e72bea9fc1df5f50132661de301d68df6f0e
lsxol <d.kazmierczak96@gmail.com>
2022-08-13 18:10:46 +0200

    Domain changes

--- src/main/java/daniel/reciperproject/controllers/IndexController.java
+++ src/main/java/daniel/reciperproject/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package daniel.reciperproject.controller;
+package daniel.reciperproject.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/daniel/reciperproject/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package daniel.reciperproject.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
