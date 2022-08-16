f7283a20de0eab34493d9cc5a78e1e7a6481302d
lsxol <d.kazmierczak96@gmail.com>
2022-08-16 12:04:35 +0200

    lombok refactoring

--- src/main/java/daniel/reciperproject/bootstrap/RecipeBootstrap.java
+++ src/main/java/daniel/reciperproject/bootstrap/RecipeBootstrap.java
@@ -4,15 +4,18 @@ import daniel.reciperproject.domain.*;
 import daniel.reciperproject.repositories.CategoryRepository;
 import daniel.reciperproject.repositories.RecipeRepository;
 import daniel.reciperproject.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -27,6 +30,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     }
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
     }
@@ -196,7 +200,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
         tacosRecipe.getCategories().add(americanCategory);
         tacosRecipe.getCategories().add(mexicanCategory);
-
+        log.debug("I am at RecipeBootstrap now");
         recipes.add(tacosRecipe);
         return recipes;
     }
