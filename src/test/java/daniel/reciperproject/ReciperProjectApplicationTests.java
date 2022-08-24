ec7c60999d24cc67fc0d7c7649fc457843660f12
lsxol <d.kazmierczak96@gmail.com>
2022-08-24 20:44:57 +0200

    testing

--- src/test/java/daniel/reciperproject/ReciperProjectApplicationTests.java
+++ src/test/java/daniel/reciperproject/ReciperProjectApplicationTests.java
@@ -1,8 +1,11 @@
 package daniel.reciperproject;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ReciperProjectApplicationTests {
 
